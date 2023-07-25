263aa3c07ae504b82ac009c794421e2f17b9192c
Hrithik Khadka <hritik.khadka@centria.fi>
2023-07-25 09:51:11 +0300

    setup a new upcoming weather component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import CurrentWeather from "./src/components/CurrentWeather";
+import UpcomingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
   return (
