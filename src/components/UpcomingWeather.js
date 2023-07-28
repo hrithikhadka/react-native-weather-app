9bbad8ed1c4a22e651fd00f9eb7ff422a451a939
Hrithik Khadka <hritik.khadka@centria.fi>
2023-07-28 15:45:31 +0300

    using flatlist to render data

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,10 +1,71 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+const DATA = [
+  {
+    dt_txt: "2023-02-18 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-02-18 15:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clouds",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-02-18 18:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Rain",
+      },
+    ],
+  },
+];
+
+const Item = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { dt_txt, min, max, condition } = props;
+  return (
+    <View>
+      <Feather name={"sun"} size={50} color={"white"} />
+      <Text>{dt_txt}</Text>
+      <Text>{min}</Text>
+      <Text>{max} </Text>
+    </View>
+  );
+};
 
 const UpcomingWeather = () => {
+  const renderItem = ({ item }) => (
+    <Item
+      condition={item.weather[0].main}
+      dt_txt={item.dt_txt}
+      min={item.main.temp_min}
+      max={item.main.temp_max}
+    />
+  );
   return (
     <SafeAreaView styles={styles.container}>
       <Text>Upcoming Weather</Text>
+      <FlatList data={DATA} renderItem={renderItem} />
     </SafeAreaView>
   );
 };
