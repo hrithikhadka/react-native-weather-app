0e8325de29558c0a6eb6464359b39a664dadb5f9
Hrithik Khadka <hritik.khadka@centria.fi>
2024-02-29 22:05:37 +0200

    Implementing the city screen

---
+++ src/screens/City.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ImageBackground,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const City = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/city.jpg")}
+        style={styles.imageLayout}
+      >
+        <Text style={[styles.cityName, styles.cityText]}>Helsinki</Text>
+        <Text style={[styles.countryName, styles.cityText]}>Finland</Text>
+        <View style={styles.populationWrapper}>
+          <Feather name={"user"} size={50} color={"red"} />
+          <Text style={styles.populationText}>50000</Text>
+        </View>
+        <View style={styles.riseSetWrapper}>
+          <Feather name={"sunrise"} size={50} color={"white"} />
+          <Text style={styles.riseText}>10:46:58am</Text>
+          <Feather name={"sunset"} size={50} color={"white"} />
+          <Text style={styles.setText}>18:28:15pm</Text>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  imageLayout: {
+    flex: 1,
+  },
+  cityName: {
+    fontSize: 40,
+  },
+  countryName: {
+    fontSize: 30,
+  },
+  cityText: {
+    justifyContent: "center",
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: "white",
+  },
+  populationWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    color: "red",
+    fontWeight: "bold",
+  },
+
+  riseSetWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 30,
+  },
+  riseText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
+
+export default City;
