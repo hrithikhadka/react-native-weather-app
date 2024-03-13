753bd8007cbc2a89188e578e78a3229de7a0b771
Hrithik Khadka <hritik.khadka@centria.fi>
2024-03-13 22:56:30 +0200

    getting users location

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  if (location) {
+    console.log(location);
+  }
   if (loading) {
     return (
       <View style={styles.container}>
@@ -12,6 +30,7 @@ const App = () => {
       </View>
     );
   }
+
   return (
     <NavigationContainer>
       <Tabs />
