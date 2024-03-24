62d1b2a0fe118bb278e2afcda1136ddcb27c1e65
Hrithik Khadka <hritik.khadka@centria.fi>
2024-03-24 22:58:11 +0200

    using moment js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import moment from "moment";
 import { weatherType } from "../utilities/weatherType";
 
 const ListItem = (props) => {
   // eslint-disable-next-line react/prop-types
   const { dt_txt, min, max, condition } = props;
-  const { item, date, temp } = styles;
+  const { item, date, temp, dateTextWrapper } = styles;
   return (
     <View style={item}>
       <Feather name={weatherType[condition].icon} size={50} color={"white"} />
-      <Text style={date}>{dt_txt}</Text>
+      <View style={styles.dateTextWrapper}>
+        <Text style={date}>{moment(dt_txt).format("dddd")}</Text>
+        <Text style={date}>{moment(dt_txt).format("h:mm:ss a")}</Text>
+      </View>
       <Text style={temp}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
     </View>
   );
@@ -35,6 +39,9 @@ const styles = StyleSheet.create({
   date: {
     color: "white",
     fontSize: 15,
+  },
+  dateTextWrapper: {
+    flexDirection: "column",
   },
 });
 
